270f1cbb4e4440a7de09a8c18d274ce723e5ee67
Chikara Matsuda <matsudachikara@yahoo.co.jp>
2016-08-02 16:12:54 +0900

    done 9.1.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,19 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+
+    else
+      render 'edit'
+    end
+  end
   
   def user_params
     params.require(:user).permit(:name,
