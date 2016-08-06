17677b0ab1fb081f233b1940b513a7187f225b98
Chikara Matsuda <matsudachikara@yahoo.co.jp>
2016-08-06 14:14:04 +0900

    Revert "done 9.1.1"
    This reverts commit 1e9c4599f2c87ed6aeccc3f0d31f0771264e77d9.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,4 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
-
   def new
     @user = User.new
   end
@@ -42,14 +40,5 @@ class UsersController < ApplicationController
                                  :email,
                                  :password,
                                  :password_confirmation)
-  end
-
-  # beforeフィルター
-  # ログイン済みユーザーかどうか確認
-  def logged_in_user
-    unless logged_in?
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
   end
 end
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -7,7 +7,6 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "unsuccessful edit" do
-    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), user:{name: "",
@@ -18,7 +17,6 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "succesful edit" do 
-    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     name = "Foo Bar"
