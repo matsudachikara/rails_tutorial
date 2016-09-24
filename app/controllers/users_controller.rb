e4f730bed59997550fad905e7d2dbc92c2c397d4
Chikara Matsuda <matsudachikara@yahoo.co.jp>
2016-09-25 01:58:39 +0900

    Revert "9.3.4 done"
    This reverts commit bda50f2af82af49b28aec4498b9bf53c8c34c6de.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
 
   def new
@@ -39,13 +39,6 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
-  end
-
-  def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User deleted"
-    logger.debug(users_url)
-    redirect_to users_url
   end
   
   def user_params
