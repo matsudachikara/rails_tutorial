5d7c0b07159919b3017b3d0c0c66e7dab08dd7b0
Chikara Matsuda <matsudachikara@yahoo.co.jp>
2016-11-13 13:49:27 +0900

    done 10.38

---
+++ db/migrate/20161113044809_add_reset_to_users.rb
@@ -0,0 +1,4 @@
+class AddResetToUsers < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160925045412) do
+ActiveRecord::Schema.define(version: 20161113044809) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
